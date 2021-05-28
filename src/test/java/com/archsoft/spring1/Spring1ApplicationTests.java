c0cd319bfa1e85a54395f16b833f74dc33c38474
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2021-05-28 18:41:42 +0200

    Force fail test

--- src/test/java/com/archsoft/spring1/Spring1ApplicationTests.java
+++ src/test/java/com/archsoft/spring1/Spring1ApplicationTests.java
@@ -1,15 +1,16 @@
 package com.archsoft.spring1;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.fail;
 
 @SpringBootTest
 class Spring1ApplicationTests {
 
 	@Test
 	void contextLoads() {
-//		Assertions.fail("I want see red ball on Jenkins");
+		fail("I want see red ball on Jenkins");
 	}
 
 }
